9864566c6471f9d5bced88a47c192df6c7f8a732
elhannaoui-ayoub <elhannaoui164@gmail.com>
2023-06-22 18:46:59 +0100

    Initial commit

---
+++ TP_MAPREDUCE/src/main/java/ma/enset/TotalPriceMapper.java
@@ -0,0 +1,18 @@
+package ma.enset;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class TotalPriceMapper extends Mapper<LongWritable, Text,Text,DoubleWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, DoubleWritable>.Context context) throws IOException, InterruptedException {
+        String[] villes = value.toString().split(" ");
+        String villeAnnee = villes[0].substring(6)+" "+villes[1];
+        context.write(new Text(villeAnnee),new DoubleWritable(Integer.parseInt(villes[3])));
+    }
+}
---
+++ TP_MAPREDUCE/src/main/java/ma/enset/TotalPriceReducer.java
@@ -0,0 +1,21 @@
+package ma.enset;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class TotalPriceReducer extends Reducer<Text, DoubleWritable,Text,DoubleWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<DoubleWritable> values, Reducer<Text, DoubleWritable, Text, DoubleWritable>.Context context) throws IOException, InterruptedException {
+       Iterator<DoubleWritable> it=values.iterator();
+       double somme=0;
+       while (it.hasNext()){
+           somme+=it.next().get();
+       }
+       context.write(key, new DoubleWritable(somme));
+    }
+}
